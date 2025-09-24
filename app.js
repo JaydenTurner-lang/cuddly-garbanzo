3e97a03416eab60466c27b6ff5e17738e2b3b14c
JaydenTurner-lang <mmajj@csc330>
2025-09-24 22:11:08 +0000

    Homework 2 finished

--- app.js
+++ app.js
@@ -1,54 +1,84 @@
 const http = require('http');
 const url = require('url');
 
-const availableTimes = {
-    Monday: ["1:00", "1:30", "2:00", "2:30", "3:00", "3:30", "4:00", "4:30"],
-    Tuesday: ["1:00", "1:30", "2:00", "2:30", "3:00", "3:30", "4:00", "4:30"],
-    Wednesday: ["1:00", "1:30", "2:00", "2:30", "3:00", "4:00", "4:30"],
-    Thursday: ["1:00", "1:30", "2:00", "2:30", "3:00", "3:30", "4:00", "4:30"],
-    Friday: ["1:30", "2:00", "2:30", "3:00", "3:30", "4:00", "4:30"],
+// Data structures
+let availableTimes = {
+  Monday: ["9:00", "10:00", "11:00"],
+  Tuesday: ["9:00", "10:00", "11:00"],
+  Wednesday: ["9:00", "10:00", "11:00"],
+  Thursday: ["9:00", "10:00", "11:00"],
+  Friday: ["9:00", "10:00", "11:00"]
 };
-const appointments = [
-    {name: "James", day: "Wednesday", time: "3:30" },
-    {name: "Lillie", day: "Friday", time: "1:00" }];
 
-let serverObj =  http.createServer(function(req,res){
-	console.log(req.url);
-	let urlObj = url.parse(req.url,true);
-	switch (urlObj.pathname) {
-		case "/schedule":
-			schedule(urlObj.query,res);
-			break;
-		case "/cancel":
-			cancel(urlObj.query,res);
-			break;
-		default:
-			error(res,404,"pathname unknown");
+let appointments = [];
 
-	}
+// Helper to send responses
+function sendResponse(res, status, message) {
+  res.writeHead(status, { "Content-Type": "text/plain" });
+  res.end(message);
+}
+
+// Route: /schedule
+function schedule(res, query) {
+  const { name, day, time } = query;
+  if (!name || !day || !time) {
+    return sendResponse(res, 400, "Missing parameters");
+  }
+  if (availableTimes[day] && availableTimes[day].includes(time)) {
+    // Reserve it
+    availableTimes[day] = availableTimes[day].filter(t => t !== time);
+    appointments.push({ name, day, time });
+    sendResponse(res, 200, "Appointment reserved");
+  } else {
+    sendResponse(res, 200, "Appointment not available");
+  }
+}
+
+// Route: /cancel
+function cancel(res, query) {
+  const { name, day, time } = query;
+  if (!name || !day || !time) {
+    return sendResponse(res, 400, "Missing parameters");
+  }
+  const index = appointments.findIndex(a => a.name === name && a.day === day && a.time === time);
+  if (index !== -1) {
+    appointments.splice(index, 1);
+    availableTimes[day].push(time);
+    sendResponse(res, 200, "Appointment has been canceled");
+  } else {
+    sendResponse(res, 404, "Appointment not found");
+  }
+}
+
+// Route: /check
+function check(res, query) {
+  const { day, time } = query;
+  if (!day || !time) {
+    return sendResponse(res, 400, "Missing parameters");
+  }
+  if (availableTimes[day] && availableTimes[day].includes(time)) {
+    sendResponse(res, 200, "Available");
+  } else {
+    sendResponse(res, 200, "Not available");
+  }
+}
+
+// Create server
+http.createServer((req, res) => {
+  const parsedUrl = url.parse(req.url, true);
+  const pathname = parsedUrl.pathname;
+  const query = parsedUrl.query;
+
+  if (pathname === "/schedule") {
+    schedule(res, query);
+  } else if (pathname === "/cancel") {
+    cancel(res, query);
+  } else if (pathname === "/check") {
+    check(res, query);
+  } else {
+    sendResponse(res, 404, "Invalid route");
+  }
+}).listen(3000, () => {
+  console.log("Server running at http://localhost:3000/");
 });
 
-function schedule(qObj,res) {
-	if (availableTimes[qObj.day].some(time => time == qObj.time))
-	{
-		res.writeHead(200,{'content-type':'text/plain'});
-		res.write("scheduled");
-		res.end();
-	}
-	else 
-		error(res,400,"Can't schedule");
-
- 
-}
-
-function cancel(qObj) {
-}
-
-function error(response,status,message) {
-
-	response.writeHead(status,{'content-type':'text/plain'});
-	response.write(message);
-	response.end();
-}
-
-serverObj.listen(80,function(){console.log("listening on port 80")});
